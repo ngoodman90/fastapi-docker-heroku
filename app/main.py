d5feb0930ae9456c4c942456d3c7656c8d2fa8c1
Noam Goodman <ngoodman@brex.com>
2022-11-18 14:05:42 +0200

    Add names

--- app/main.py
+++ app/main.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 
-app = FastAPI()
+app = FastAPI(docs_url="/api/docs")
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get("/names")
+async def root():
+    return {"message": "Uri & Noam"}
+
+
